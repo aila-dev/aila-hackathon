113d47e668e33537b0ab792937d2da7bfc10d47e
ardli-firman <ardlifirman17@gmail.com>
2019-10-15 02:37:28 +0700

    Minum

--- app/src/main/java/com/aila/ailahackathon/model/MinumModel.java
+++ app/src/main/java/com/aila/ailahackathon/model/MinumModel.java
@@ -1,4 +1,33 @@
 package com.aila.ailahackathon.model;
 
+import com.google.firebase.Timestamp;
+
 public class MinumModel {
+    private int minum_perhari;
+    private Timestamp tanggal;
+    private int total_minum;
+
+    public int getMinum_perhari() {
+        return minum_perhari;
+    }
+
+    public void setMinum_perhari(int minum_perhari) {
+        this.minum_perhari = minum_perhari;
+    }
+
+    public int getTotal_minum() {
+        return total_minum;
+    }
+
+    public void setTotal_minum(int total_minum) {
+        this.total_minum = total_minum;
+    }
+
+    public Timestamp getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(Timestamp tanggal) {
+        this.tanggal = tanggal;
+    }
 }
