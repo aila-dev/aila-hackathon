a6ca598d5dd6aaed12f007cea8b81fc5f8730b60
ardli-firman <ardlifirman17@gmail.com>
2019-10-15 04:52:45 +0700

    Fix list minum

--- app/src/main/java/com/aila/ailahackathon/fragment/TabFour.java
+++ app/src/main/java/com/aila/ailahackathon/fragment/TabFour.java
@@ -4,6 +4,7 @@ package com.aila.ailahackathon.fragment;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,6 +15,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import com.aila.ailahackathon.MainTab;
 import com.aila.ailahackathon.R;
 import com.aila.ailahackathon.auth.Registration;
 import com.aila.ailahackathon.fragment.minum.MinumAdapter;
@@ -33,11 +35,16 @@ import java.util.List;
  * A simple {@link Fragment} subclass.
  */
 public class TabFour extends Fragment {
-    private List<MinumModel> listMinum = new ArrayList<MinumModel>();
+    List<MinumModel> listMinum = new ArrayList<>();
     private RecyclerView recyclerView;
     private MinumAdapter minumAdapter;
     public TabFour() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
     }
 
     @Override
@@ -56,6 +63,9 @@ public class TabFour extends Fragment {
                             listMinum.add(minumModel);
                         }
                         minumAdapter = new MinumAdapter(listMinum);
+                        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(getContext());
+                        recyclerView.setLayoutManager(mLayoutManager);
+                        recyclerView.setItemAnimator(new DefaultItemAnimator());
                         recyclerView.setAdapter(minumAdapter);
                     }
                 }).addOnFailureListener(new OnFailureListener() {
@@ -70,7 +80,9 @@ public class TabFour extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_four, container, false);
+        View view = inflater.inflate(R.layout.fragment_tab_four, container, false);
+        recyclerView = view.findViewById(R.id.recyclerMinum);
+        return view;
     }
 
 }
