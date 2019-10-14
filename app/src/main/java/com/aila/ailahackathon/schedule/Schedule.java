796ccfbd44214723d8e4a1c892423bf7a187bbc2
renryuuka <vembrivrd@gmail.com>
2019-10-14 12:14:25 +0700

    Package

---
+++ app/src/main/java/com/aila/ailahackathon/schedule/Schedule.java
@@ -0,0 +1,16 @@
+package com.aila.ailahackathon.schedule;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aila.ailahackathon.R;
+
+public class Schedule extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_schedule);
+    }
+}
