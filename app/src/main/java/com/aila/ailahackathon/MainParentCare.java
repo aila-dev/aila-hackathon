9e968f2072b4d0659908ddac68e06a1e470a2339
renryuuka <vembrivrd@gmail.com>
2019-10-14 21:41:29 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/aila/ailahackathon/AILAFloat.java

--- app/src/main/java/com/aila/ailahackathon/MainParentCare.java
+++ app/src/main/java/com/aila/ailahackathon/MainParentCare.java
@@ -11,36 +11,36 @@ import com.aila.ailahackathon.auth.Login;
 import com.aila.ailahackathon.auth.Registration;
 
 public class MainParentCare extends AppCompatActivity {
-    Button login,regist;
+//    Button login,regist;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_parent_care);
-        login=findViewById(R.id.tologin);
-        regist=findViewById(R.id.toregist);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in=new Intent(getBaseContext(), Login.class);
-                startActivity(in);
-            }
-        });
-        regist.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent in=new Intent(getBaseContext(), Registration.class);
-                startActivity(in);
-            }
-        });
+//        login=findViewById(R.id.tologin);
+//        regist=findViewById(R.id.toregist);
+//        login.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent in=new Intent(getBaseContext(), Login.class);
+//                startActivity(in);
+//            }
+//        });
+//        regist.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent in=new Intent(getBaseContext(), Registration.class);
+//                startActivity(in);
+//            }
+//        });
 
     }
 
-//    public void movelogin(View view){
-//        Intent in=new Intent(getBaseContext(), Login.class);
-//        startActivity(in);
-//    }
-//    public void moveregistration(View view){
-//        Intent in=new Intent(getBaseContext(), Registration.class);
-//        startActivity(in);
-//    }
+    public void movelogin(View view){
+        Intent in=new Intent(getBaseContext(), Login.class);
+        startActivity(in);
+    }
+    public void moveregistration(View view){
+        Intent in=new Intent(getBaseContext(), Registration.class);
+        startActivity(in);
+    }
 }
