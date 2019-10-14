ef4e1442361e9173662e5c17b83c154c9b4b7474
renryuuka <vembrivrd@gmail.com>
2019-10-14 12:26:36 +0700

    Package

---
+++ app/src/main/java/com/aila/ailahackathon/auth/Registration.java
@@ -0,0 +1,16 @@
+package com.aila.ailahackathon.auth;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aila.ailahackathon.R;
+
+public class Registration extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+}
