635097d788678be20aa98352f9953435effa050d
pranendra <samuel.benner@my.gavilan.edu>
2019-10-14 22:03:41 +0700

    Inspector

--- app/src/main/java/com/aila/ailahackathon/inspector/Inspector.java
+++ app/src/main/java/com/aila/ailahackathon/inspector/Inspector.java
@@ -2,15 +2,39 @@ package com.aila.ailahackathon.inspector;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
 
 import com.aila.ailahackathon.R;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lecho.lib.hellocharts.model.PieChartData;
+import lecho.lib.hellocharts.model.SliceValue;
+import lecho.lib.hellocharts.view.PieChartView;
+
 public class Inspector extends AppCompatActivity {
+
     private static final String TAG = "Inspector";
+    PieChartView pieChartView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inspector);
+
+        pieChartView = findViewById(R.id.pieChart);
+
+        List pieData = new ArrayList<>();
+        pieData.add(new SliceValue(15, Color.BLUE).setLabel("Q1: $10"));
+        pieData.add(new SliceValue(25, Color.GRAY).setLabel("Q2: $4"));
+        pieData.add(new SliceValue(10, Color.RED).setLabel("Q3: $18"));
+        pieData.add(new SliceValue(60, Color.MAGENTA).setLabel("Q4: $28"));
+
+        PieChartData pieChartData = new PieChartData(pieData);
+        pieChartData.setHasLabels(true).setValueLabelTextSize(14);
+        pieChartData.setHasCenterCircle(true).setCenterText1("Sales in million").setCenterText1FontSize(20).setCenterText1Color(Color.parseColor("#0097A7"));
+        pieChartView.setPieChartData(pieChartData);
     }
 }
