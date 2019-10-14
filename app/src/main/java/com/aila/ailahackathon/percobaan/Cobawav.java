63e79696ef81da2259057af6f31b8805941906c9
pranendra <samuel.benner@my.gavilan.edu>
2019-10-14 14:07:48 +0700

    Merge branch 'master' of /home/evanz/Documents/aila-hackathon with conflicts.

---
+++ app/src/main/java/com/aila/ailahackathon/percobaan/Cobawav.java
@@ -0,0 +1,16 @@
+package com.aila.ailahackathon.percobaan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aila.ailahackathon.R;
+
+public class Cobawav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cobawav);
+    }
+}
