e90bcf7c398e1b72b6b1782680d703be51634435
renryuuka <vembrivrd@gmail.com>
2019-10-14 16:48:36 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/aila/ailahackathon/AILAFloat.java

--- app/src/main/java/com/aila/ailahackathon/MainActivity.java
+++ app/src/main/java/com/aila/ailahackathon/MainActivity.java
@@ -1,39 +1,55 @@
 package com.aila.ailahackathon;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.provider.Settings;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.Toast;
-
-import com.aila.ailahackathon.inspector.Inspector;
-import com.aila.ailahackathon.schedule.Schedule;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private static final int SYSTEM_ALERT_WINDOW_PERMISSION = 7;
 
+    ImageView imageView;
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.activeaila);
+        if (!Settings.canDrawOverlays(this)) {
+            RuntimePermissionForUser();
+        }
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.M) {
+                    startService(new Intent(MainActivity.this, Aila.class));
+                    finish();
+                } else if (Settings.canDrawOverlays(MainActivity.this)) {
+                    startService(new Intent(MainActivity.this, Aila.class));
+                } else {
+                    RuntimePermissionForUser();
+                    Toast.makeText(MainActivity.this, "System Alert Window Permission is Required For Floating WIdget.", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
     public void moveparentcare(View view){
         Intent in=new Intent(getBaseContext(),MainParentCare.class);
         startActivity(in);
     }
     public void RuntimePermissionForUser() {
-        Intent PermissionIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
+        Intent PermissionIntent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:" + getPackageName()));
+
         startActivityForResult(PermissionIntent, SYSTEM_ALERT_WINDOW_PERMISSION);
-    }
-    public void activateaila(View view){
-        startService(new Intent(MainActivity.this, Aila.class));
-        if(SYSTEM_ALERT_WINDOW_PERMISSION!=7) {
-            RuntimePermissionForUser();
-            Toast.makeText(MainActivity.this, "System Alert Window Permission is Required For Floating WIdget.", Toast.LENGTH_LONG).show();
-        }
     }
 }
