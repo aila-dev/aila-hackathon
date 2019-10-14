e72d4fbc5684902b7c63e901186f859d2bb7ba5d
renryuuka <vembrivrd@gmail.com>
2019-10-15 01:13:28 +0700

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/aila/ailahackathon/AILAFloat.java

---
+++ app/src/main/java/com/aila/ailahackathon/fragment/TabTwo.java
@@ -0,0 +1,29 @@
+package com.aila.ailahackathon.fragment;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.aila.ailahackathon.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class TabTwo extends Fragment {
+
+
+    public TabTwo() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_tab_two, container, false);
+    }
+
+}
