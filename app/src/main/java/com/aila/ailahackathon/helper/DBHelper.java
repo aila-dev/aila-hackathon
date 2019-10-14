b3a519535408d7cc6481e5f7270c784877b7abdf
ardli-firman <ardlifirman17@gmail.com>
2019-10-14 12:26:05 +0700

    Add

--- app/src/main/java/com/aila/ailahackathon/helper/DBHelper.java
+++ app/src/main/java/com/aila/ailahackathon/helper/DBHelper.java
@@ -5,6 +5,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 public class DBHelper extends SQLiteOpenHelper {
+    private static final String TAG = "DBHelper";
     private static final String DB_NAME = "aila.db";
     private static final int DB_VERSION = 1;
 
