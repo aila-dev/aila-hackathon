e5540a7d7b634ce2ac6a9a51fd3967c857c079e5
pranendra <samuel.benner@my.gavilan.edu>
2019-10-15 07:59:23 +0700

    VideoFaceDetection

---
+++ app/src/main/java/com/aila/ailahackathon/percobaan/VideoFaceDetection.java
@@ -0,0 +1,16 @@
+ package com.aila.ailahackathon.percobaan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.aila.ailahackathon.R;
+
+ public class VideoFaceDetection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_video_face_detection);
+    }
+}
