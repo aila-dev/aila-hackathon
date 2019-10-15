0bf57534cf6359a084d4752f5eb9fa8dd0cef6d8
ardli-firman <ardlifirman17@gmail.com>
2019-10-15 08:01:29 +0700

    Alarm

--- app/src/main/java/com/aila/ailahackathon/schedule/ScheduleBroadcastReciever.java
+++ app/src/main/java/com/aila/ailahackathon/schedule/ScheduleBroadcastReciever.java
@@ -3,10 +3,11 @@ package com.aila.ailahackathon.schedule;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.widget.Toast;
 
 public class ScheduleBroadcastReciever extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        Toast.makeText(context,"Trigger",Toast.LENGTH_LONG).show();
     }
 }
